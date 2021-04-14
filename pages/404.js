0d750563c3ced7d714aa324c2be1428163fd43f0
Yosif Georgiev <iodesign@live.com>
2021-04-14 14:10:59 +0300

    404 page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-const ErrorPage = () => {
-    return ( 
-        <div>
-            <h1>Page not found! Error 404!</h1>
-        </div>
-     );
+const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 4000)
+  }, [])
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Oops! That page cannot be found :(</h2>
+      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> for more marmite goodness...</p>
+
+      <style jsx>{`
+        .not-found {
+          background: #fff;
+          padding: 30px;
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+          transform: rotateZ(-1deg);
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  );
 }
  
-export default ErrorPage;
+export default NotFound;
